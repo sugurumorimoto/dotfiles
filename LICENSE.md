37139969344334f3b2befe27b6bdfd64efa6e862
sugurumorimoto <woodwood0809@icloud.com>
2022-08-01 02:09:22 +0900

    初回コミット

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 # MIT License (MIT)
 
-Copyright (c) 2022 NagayamaRyoga
+Copyright (c) 2022 sugurumorimoto
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-INSTALL_DIR="${INSTALL_DIR:-$HOME/repos/github.com/NagayamaRyoga/dotfiles}"
+INSTALL_DIR="${INSTALL_DIR:-$HOME/repos/github.com/sugurumorimoto/dotfiles}"
 
 if [ -d "$INSTALL_DIR" ]; then
     echo "Updating dotfiles..."
     git -C "$INSTALL_DIR" pull
 else
     echo "Installing dotfiles..."
-    git clone https://github.com/NagayamaRyoga/dotfiles "$INSTALL_DIR"
+    git clone https://github.com/sugurumorimoto/dotfiles "$INSTALL_DIR"
 fi
 
 /bin/bash "$INSTALL_DIR/scripts/setup.bash"
