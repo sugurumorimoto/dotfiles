06881e4ec9016ef366963ab010f228ed4d7e9419
sugurumorimoto <woodwood0809@icloud.com>
2024-04-10 12:29:06 +0900

    Update .gitignore and .zshrc.lazy files

--- scripts/setup-links.bash
+++ scripts/setup-links.bash
@@ -25,7 +25,10 @@ ln -sfv "$XDG_CONFIG_HOME/zsh/.zprofile" "$HOME/.zprofile"
 ln -sfv "$XDG_CONFIG_HOME/zsh/.zshrc" "$HOME/.zshrc"
 ln -sfv "$XDG_CONFIG_HOME/zsh/.zshrc.lazy" "$HOME/.zshrc.lazy"
 ln -sfv "$XDG_CONFIG_HOME/R/.radina_profile" "$HOME/.radian_profile"
+
 ln -sfv "$XDG_CONFIG_HOME/git/template/commit-template.txt" "$REPO_DIR/.git/commit-template.txt"
+ln -sfv "$XDG_CONFIG_HOME/git/ignore" "$HOME/.gitignore"
+
 ln -sfv "$XDG_CONFIG_HOME/editorconfig/.editorconfig" "$HOME/.editorconfig"
 ln -sfnv "$XDG_CONFIG_HOME/vim" "$HOME/.vim"
 ln -sfv "$XDG_CONFIG_HOME/commitizen/.czrc" "$HOME/.czrc"
